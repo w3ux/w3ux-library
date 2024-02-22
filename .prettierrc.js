77680f2d2b870031ae25d278d9c4d71191401edc
Ross Bulat <ross@parity.io>
2024-02-22 19:34:22 +0700

    chore: add project config

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+/* @license Copyright 2024 @polkadot-cloud/library authors & contributors
+SPDX-License-Identifier: GPL-3.0-only */
+
+module.exports = {
+  arrowParens: "always",
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+};
