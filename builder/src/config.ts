f817820f77ca3a1787e2c1e09528276d81f60855
Ross Bulat <ross@parity.io>
2024-02-24 15:57:16 +0700

    minimise comment footprint

--- builder/src/config.ts
+++ builder/src/config.ts
@@ -1,14 +1,11 @@
-// ----------------------------
 // Package scope to publish to.
 // ----------------------------
 export const PACKAGE_SCOPE = "w3ux";
 
-// ----------------------------
 // Package build output folder.
 // ----------------------------
 export const PACKAGE_OUTPUT = "dist";
 
-// ----------------------------------------------
 // Files that are required to exist in a package.
 // ----------------------------------------------
 export const PACKAGE_SOURCE_REQUIRED_FILES = [
@@ -16,7 +13,6 @@ export const PACKAGE_SOURCE_REQUIRED_FILES = [
   "package.json",
 ];
 
-// --------------------------------------------------------------
 // Required package.json properties to copy to the package build.
 // --------------------------------------------------------------
 export const PACKAGE_SOURCE_REQUIRED_PROPERTIES = ["license", "version"];
