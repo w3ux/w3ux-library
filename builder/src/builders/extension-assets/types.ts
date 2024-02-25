0110af1cc2909b94fe9e2b5fa47cb5995ed115f4
Ross Bulat <ross@parity.io>
2024-02-25 17:49:26 +0700

    amend config

--- builder/src/builders/extension-assets/types.ts
+++ builder/src/builders/extension-assets/types.ts
@@ -1,28 +1,7 @@
 /* @license Copyright 2024 w3ux authors & contributors
 SPDX-License-Identifier: GPL-3.0-only */
 
-import { CSSProperties, FC } from "react";
-
-// Structure for an extension configuration.
-export interface ExtensionConfig {
-  title: string;
-  category: "web-extension" | "hardware";
-  website:
-    | string
-    | {
-        url: string;
-        text: string;
-      };
-  features: "*" | ExtensionFeature[];
+export interface AdditionalAsset {
+  input: string;
+  outputFilename: string;
 }
-
-// Supported extension features.
-export type ExtensionFeature = "getAccounts" | "subscribeAccounts" | "signer";
-
-// Icon record structure.
-export type ExtensionIconRecords = Record<string, ExtensionIcon>;
-
-export type ExtensionIcon = FC<{
-  style?: CSSProperties;
-  className?: string;
-}>;
