714e4f54ecfcd18aa60dd7fc758648af4b04beca
Ross Bulat <ross@parity.io>
2024-02-25 14:27:12 +0700

    add licenses

--- builder/src/builders/extension-assets/index.ts
+++ builder/src/builders/extension-assets/index.ts
@@ -1,3 +1,6 @@
+/* @license Copyright 2024 w3ux authors & contributors
+SPDX-License-Identifier: GPL-3.0-only */
+
 import fs from "fs/promises";
 import { join } from "path";
 import { PACKAGE_OUTPUT } from "config";
--- builder/src/builders/utils.ts
+++ builder/src/builders/utils.ts
@@ -1,3 +1,6 @@
+/* @license Copyright 2024 w3ux authors & contributors
+SPDX-License-Identifier: GPL-3.0-only */
+
 import { PACKAGE_OUTPUT } from "config";
 import fs from "fs/promises";
 import { join, dirname } from "path";
