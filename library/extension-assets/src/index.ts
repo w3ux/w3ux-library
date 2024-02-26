36cfda98021eadd6998728b3ff69cea6b625e7ae
Ross Bulat <ross@parity.io>
2024-02-26 17:06:05 +0700

    chore: move index

--- library/extension-assets/src/index.ts
+++ library/extension-assets/src/index.ts
@@ -1,4 +1,4 @@
 /* @license Copyright 2024 w3ux authors & contributors
 SPDX-License-Identifier: GPL-3.0-only */
 
-// NOTE: empty file to surpress `tsconfig.json` warning.
+// NOTE: placeholder to surpress `tsconfig.json` no input warning.
