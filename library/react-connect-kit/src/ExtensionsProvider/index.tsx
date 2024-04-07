7392bdbc77cd15416fb007370864bcb6f3f8920f
G. Kami Ekbatanifard <46442452+Nick-1979@users.noreply.github.com>
2024-04-07 11:28:56 +0330

    To just enable Polkagate Snape (#34)

--- library/react-connect-kit/src/ExtensionsProvider/index.tsx
+++ library/react-connect-kit/src/ExtensionsProvider/index.tsx
@@ -81,7 +81,7 @@ export const ExtensionsProvider = ({
 
     // Inject PolkaGate Snap if enabled.
     if (polkaGateSnapEnabled) {
-      await withTimeout(500, web3Enable("w3ux"));
+      await withTimeout(500, web3Enable("snap_only"));
     }
 
     if (hasInjectedWeb3 || snapAvailable) {
