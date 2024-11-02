06ec2ca6a60a8fed668cb274816d83655b27de57
Ross Bulat <ross@jkrb.io>
2024-11-02 14:30:17 +0700

    chore: use generic

--- library/factories/src/withProviders.tsx
+++ library/factories/src/withProviders.tsx
@@ -4,11 +4,11 @@ SPDX-License-Identifier: GPL-3.0-only */
 import type { FC } from "react";
 
 // `providers` accepts standalone functional components or an array of a functional component and its props.
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type Provider = FC<any> | [FC<any>, any];
+export type Provider<T> = FC<T> | [FC<T>, T];
 
 // A pure function that applies an arbitrary amount of context providers to a wrapped component.
-export const withProviders = (providers: Provider[], Wrapped: FC) =>
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const withProviders = (providers: Provider<any>[], Wrapped: FC) =>
   providers.reduceRight(
     (acc, prov) => {
       if (Array.isArray(prov)) {
