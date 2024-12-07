29c8df9499dea62c45d53cbe407692e743de112b
helixstreet <53312868+helixstreet@users.noreply.github.com>
2024-12-07 16:21:48 +0100

    Update library/validator-assets/src/helixstreet.tsx
    Co-authored-by: Ross Bulat <ross@jkrb.io>

--- library/validator-assets/src/helixstreet.tsx
+++ library/validator-assets/src/helixstreet.tsx
@@ -1,4 +1,4 @@
-const helixstreet = () => (
+const Helixstreet = () => (
 
     <? xml version = "1.0" encoding = "UTF-8" standalone = "no" ?>
     < !--Created with Inkscape(http://www.inkscape.org/) -->
