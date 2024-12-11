a5674e20a8e3cad16cc227786efcd390e9e32dab
Guru <fazakis@ece.upatras.gr>
2024-12-11 12:05:40 +0200

    Config for GuruStaking validators

---
+++ library/validator-assets/src/GuruStaking.tsx
@@ -0,0 +1,10 @@
+const GuruStaking = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 350 350">
+      <rect x="0" y="0" width="350" height="350" fill="white" />
+      <text x="50%" y="50%" font-size="290" text-anchor="middle" dominant-baseline="central" fill="black">
+        👳
+      </text>
+  </svg>
+);
+
+export default GuruStaking;
