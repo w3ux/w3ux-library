6d0cc41040c7f45a99b2eb2a95374785fc13f3f4
GuruStakingCom <141773557+GuruStakingCom@users.noreply.github.com>
2024-12-11 12:08:30 +0200

    small fix and clean up icon code

--- library/validator-assets/src/GuruStaking.tsx
+++ library/validator-assets/src/GuruStaking.tsx
@@ -1,9 +1,7 @@
 const GuruStaking = () => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 350 350">
       <rect x="0" y="0" width="350" height="350" fill="white" />
-      <text x="50%" y="50%" font-size="290" text-anchor="middle" dominant-baseline="central" fill="black">
-        👳
-      </text>
+      <text x="50%" y="50%" font-size="290" text-anchor="middle" dominant-baseline="central" fill="white">👳</text>
   </svg>
 );
 
