0944377f7c6aa39edb85c0cb9ce197a878617c21
Ross Bulat <ross@jkrb.io>
2025-03-31 12:28:13 +0700

    chore: amend types & bump

--- library/types/src/common.ts
+++ library/types/src/common.ts
@@ -1,8 +1,6 @@
 /* @license Copyright 2024 w3ux authors & contributors
 SPDX-License-Identifier: GPL-3.0-only */
 
-import { CSSProperties, ReactNode } from "react";
-
 // An optional string.
 export type MaybeString = string | null;
 
@@ -12,24 +10,6 @@ export type HexString = `0x${string}`;
 // A general purpose sync status, tracking whether an item is not synced, in progress, or completed
 // syncing. Useful for tracking async function progress, used as React refs, etc.
 export type Sync = "synced" | "unsynced" | "syncing";
-
-// A general purpose medium components are being displayed on.
-export type DisplayFor = "default" | "modal" | "canvas" | "card";
-
-// A generic type for basic React components. We assume the component may have children and styling
-// applied to it.
-export interface ComponentBase {
-  // passing react children.
-  children?: ReactNode;
-  // passing custom styling.
-  style?: CSSProperties;
-}
-
-// An extension of the ComponentBase type with an additional className property.
-export type ComponentBaseWithClassName = ComponentBase & {
-  // passing a className string.
-  className?: string;
-};
 
 // A funtion with no arguments and no return value.
 export type VoidFn = () => void;
