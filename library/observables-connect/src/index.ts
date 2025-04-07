62958ee97bd6af77148db2328c738aca46856cd2
Ross Bulat <ross@jkrb.io>
2025-04-07 21:20:57 +0700

    rename

--- library/observables-connect/src/index.ts
+++ library/observables-connect/src/index.ts
@@ -6,7 +6,7 @@ import { BehaviorSubject } from 'rxjs'
 
 // Whether extensions are being checked
 export const _gettingExtensions = new BehaviorSubject<boolean>(true)
-export const checkingExtensions$ = _gettingExtensions.asObservable()
+export const gettingExtensions$ = _gettingExtensions.asObservable()
 
 // Discovered extensions along with their status
 export const _extensionsStatus = new BehaviorSubject<ExtensionsStatus>({})
