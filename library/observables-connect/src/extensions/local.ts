11c3f0b96f0557da29e90856834edb31cb26e93c
Ross Bulat <ross@jkrb.io>
2025-04-08 15:31:32 +0700

    mv consts to src

--- library/observables-connect/src/extensions/local.ts
+++ library/observables-connect/src/extensions/local.ts
@@ -2,7 +2,7 @@
 SPDX-License-Identifier: GPL-3.0-only */
 
 import { localStorageOrDefault } from '@w3ux/utils'
-import { ActiveExtensionsKey } from '../accounts/consts'
+import { ActiveExtensionsKey } from '../consts'
 
 // Check if an extension exists in local storage
 export const isExtensionLocal = (id: string): boolean => {
