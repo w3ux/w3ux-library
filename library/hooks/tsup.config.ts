c3d1f23cdc26bdb98aac641f250d8143e6a1c446
Ross Bulat <ross@jkrb.io>
2025-06-11 18:47:55 +0700

    sourcemaps to `hooks`

--- library/hooks/tsup.config.ts
+++ library/hooks/tsup.config.ts
@@ -4,11 +4,11 @@ SPDX-License-Identifier: GPL-3.0-only */
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.tsx'],
+  entry: ['src/index.tsx', 'src/util.ts'],
   target: 'esnext',
-  splitting: false,
-  sourcemap: false,
+  sourcemap: true,
   clean: true,
   dts: true,
   format: ['esm', 'cjs'],
+  external: ['react', 'react-dom'],
 })
