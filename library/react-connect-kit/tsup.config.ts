583088de55cda35003c142ab5116470c795422ef
Ross Bulat <ross@jkrb.io>
2025-06-11 18:55:04 +0700

    sourcemaps to observables, connect kit

--- library/react-connect-kit/tsup.config.ts
+++ library/react-connect-kit/tsup.config.ts
@@ -6,8 +6,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['src/index.tsx'],
   target: 'esnext',
-  splitting: false,
-  sourcemap: false,
+  sourcemap: true,
   clean: true,
   dts: true,
   format: ['esm', 'cjs'],
