0035f40fcd7d533ad3376e610be0681c70dc1358
Ross Bulat <ross@jkrb.io>
2026-04-04 15:21:20 +0700

    chore: fix import

--- builder/src/index.ts
+++ builder/src/index.ts
@@ -2,8 +2,8 @@
 /* @license Copyright 2024 w3ux authors & contributors
 SPDX-License-Identifier: GPL-3.0-only */
 
-import { simpleBuild } from 'builders/common/simpleBuild'
 import minimist from 'minimist'
+import { simpleBuild } from './builders/common/simpleBuild'
 import { build as buildDirectory } from './builders/directory'
 import { build as buildExtensionAssets } from './builders/extension-assets'
 import { build as buildPackageReadmes } from './builders/package-readme'
